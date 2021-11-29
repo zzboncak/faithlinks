d0408a9d31137b69c5de866168ba188862bb04a3
zzboncak <zachary.zboncak@gmail.com>
2021-11-29 12:37:27 -0600

    got rid of unused components from tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./views/Home";
-import About from "./views/About";
-import Foods from "./views/Foods";
-import FilteredFoods from "./views/FilteredFoods";
-import Header from "./components/Header";
 import "./App.css";
 
 function App() {
